2a67b150cd56a9a46bf159d80ee20357b1e68947
Ho Yin Keung <hykeung92@gmail.com>
2019-12-10 17:54:25 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -10,7 +10,7 @@
 
 def speak_to_grandma(phrase)
   #if you say I LOVE YOU GRANDMA
-  if phrase == "I LOVE YOU GRANDMA"
+  if phrase == "I LOVE YOU GRANDMA!"
   #responds with "I LOVE YOU TOO PUMPKIN"
     return "I LOVE YOU TOO PUMPKIN!"
     
